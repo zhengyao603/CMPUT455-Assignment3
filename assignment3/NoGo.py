854e03d1a70d584830ce1b3e6f6b2772bf77bd58
zhengyao603 <zhengyao@ualberta.ca>
2022-03-08 23:53:38 -0700

    Update NoGo.py

--- assignment3/NoGo.py
+++ assignment3/NoGo.py
@@ -6,6 +6,9 @@ from platform import win32_ver
 from gtp_connection import GtpConnection
 from board_util import GoBoardUtil
 from board import GoBoard
+from math import log, sqrt
+import sys
+import random
 
 
 class Go0:
@@ -25,7 +28,17 @@ class Go0:
         self.selection = 'rr'
         self.policy = 'random'
         self.simulations = 10
-        self.weights = None
+        self.weights = self.open_file('weights')
+    
+    def open_file(self, file_name):
+        weights = dict()
+        f = open(file_name, 'r')
+        for line in f:
+            data = line.split(' ')
+            weights[int(data[0])] = float(data[1])
+        f.close()
+
+        return weights
     
     def simulate(self, board, move, toplay):
         board_copy = board.copy()
@@ -43,25 +56,87 @@ class Go0:
     def play_game(self, board):
         if self.policy == 'random':
             while 1:
-                move = GoBoardUtil.generate_random_move(board, board.current_player)
+                player = board.current_player
+                move = GoBoardUtil.generate_random_move(board, player)
                 if not move:
-                    return GoBoardUtil.opponent(board.current_player)
-                board.play_move(move, board.current_player)
+                    return GoBoardUtil.opponent(player)
+                board.play_move(move, player)
 
         elif self.policy == 'pattern':
-            pass
+            while 1:
+                player = board.current_player
+                moves = GoBoardUtil.generate_legal_moves(board, player)
+                if len(moves) == 0:
+                    return GoBoardUtil.opponent(player)
+                
+                move_weights = dict()
+                weight_sum = 0
+                for move in moves:
+                    grid = [move+board.NS-1, move+board.NS, move+board.NS+1, move-1, move+1, move-board.NS-1, move-board.NS, move-board.NS+1]
+                    weight = 0
+                    for i in range(len(grid)):
+                        value = board.board[grid[i]]
+                        weight += value * (4 ** i)
+                    move_weight = self.weights.get(weight)
+                    move_weights[move] = move_weight
+                    weight_sum += move_weight
+                
+                for key in move_weights.keys():
+                    move_weights[key] = move_weights[key]/weight_sum
+                
+                prob = random.uniform(0,1)
+                for move in move_weights.keys():
+                    if prob <= move_weights[move]:
+                        board.play_move(move, player)
+                        break
+
 
     def get_move(self, board, color):
         legal_moves = GoBoardUtil.generate_legal_moves(board, color)
+        sims = len(legal_moves) * 10
         probablity = {}
+        best = -1
+        best_move = None
 
         if self.selection == 'rr':
             for move in legal_moves:
-                self.simulate_move(board, move, color)
+                wins = self.simulate_move(board, move, color)
+                probablity[move] = wins/sims
+            for move in probablity.keys():
+                if probablity[move] > best:
+                    best = probablity[move]
+                    best_move = move
+            return best_move
         
         elif self.selection == 'ucb':
-            pass
+            C = 0.4
+            stats = []
+            for i in range(len(legal_moves)):
+                stats.append([0,0])
+            for n in len(sims):
+                max = -1
+                max_score = -float("inf")
+                for j in range(len(stats)):
+                    if stats[j][1] == 0:
+                        score = float("inf")
+                    else:
+                        score = (stats[j][0] / stats[j][1]) + C * sqrt(log(n) / stats[j][1])
+                    if score > max_score:
+                        max_score = score
+                        max = j
+                
+                result = self.simulate(board, max, color)
+                if result == color:
+                    stats[max][0] += 1
+                stats[max][1] += 1
 
+                max_arm = -1
+                max_arm_count = -float("inf")
+                for i in range(len(stats)):
+                    if stats[i][1] > max_arm_count:
+                        max_arm = i
+                        max_arm_count = stats[i][1]
+                return max_arm
 
 def run():
     """
